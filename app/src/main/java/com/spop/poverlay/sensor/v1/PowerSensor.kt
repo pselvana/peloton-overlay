ce7a31c56572c4571fbda2c3b9763f733f5d784c
Piranavan Selvanandan <pirana@gmail.com>
2025-04-08 18:47:38 -0700

    Update PowerSensor.kt to remove /100

--- app/src/main/java/com/spop/poverlay/sensor/v1/PowerSensor.kt
+++ app/src/main/java/com/spop/poverlay/sensor/v1/PowerSensor.kt
@@ -9,5 +9,6 @@ import timber.log.Timber
  * These spikes are referred to as "Spurious Readings" in this implementation, and rejected
  */
 class PowerSensor(binder: IBinder) : Sensor(Command.GetPowerRepeating, binder) {
-    override fun mapValue(value: Float) = value / 100
+    override fun mapValue(value: Float) = value 
+    /* removing "/ 100"  since originally implementation doesn't look like this https://github.com/selalipop/grupetto/blob/d6f96d4d78598a4c2d33ce597e2b041891643d07/app/src/main/java/com/spop/poverlay/sensor/PowerSensor.kt */
 }
